b2ea848e3f3a8fc40db9d8a24217371629be5804
h4ck0lympus <vishaljuneja707@gmail.com>
2025-07-09 07:12:24 +0200

    idk

--- src/app/tilefuzz/driver.c
+++ src/app/tilefuzz/driver.c
@@ -354,8 +354,8 @@ isolated_shred_topo( config_t * config, fd_topo_obj_callbacks_t * callbacks[] ) 
 
   /* TODO explore using less memory for the tiles that are purely
           mocks */
+  /* TODO explore saving memory by using just one wksp for all links */
   fd_topob_wksp    ( topo, "shred_store" );
-  // fd_topob_link    ( topo, "shred_store", "shred_store", 65536UL, 2048UL, 1UL );
   fd_topob_link( topo, "shred_store",  "shred_store",  65536UL, 4UL*FD_SHRED_STORE_MTU, 4UL+config->tiles.shred.max_pending_shred_sets );
   fd_topob_tile_out( topo, "shred", 0UL, "shred_store", 0UL );
 
@@ -399,86 +399,6 @@ isolated_shred_topo( config_t * config, fd_topo_obj_callbacks_t * callbacks[] ) 
   (void)replay_tile;
 
   fd_topob_finish( topo, callbacks );
-}
-
-/* Maybe similar to what initialize workspaces does, without
-   following it closely */
-static void
-back_wksps( fd_topo_t * topo, fd_topo_obj_callbacks_t * callbacks[] ) {
-  ulong keyswitch_obj_id = ULONG_MAX;
-  for( ulong i=0UL; i<topo->obj_cnt; i++ ) {
-    fd_topo_obj_t * obj = &topo->objs[ i ];
-    fd_topo_obj_callbacks_t * cb = NULL;
-    for( ulong j=0UL; callbacks[ j ]; j++ ) {
-      if( FD_UNLIKELY( !strcmp( callbacks[ j ]->name, obj->name ) ) ) {
-        cb = callbacks[ j ];
-        break;
-      }
-    }
-    ulong align = cb->align( topo, obj );
-
-    obj->wksp_id = obj->id;
-    topo->workspaces[ obj->wksp_id ].wksp = aligned_alloc( align, obj->footprint );
-    obj->offset = 0UL;
-    FD_LOG_NOTICE(( "obj %s %lu %lu %lu %lu", obj->name, obj->wksp_id, obj->footprint, obj->offset, align ));
-    FD_LOG_NOTICE(( "wksp pointer %p", (void*)topo->workspaces[ obj->wksp_id ].wksp ));
-    /* ~equivalent to fd_topo_wksp_new in a world of real workspaces */
-    if( FD_UNLIKELY( cb->new ) ) { /* only saw this null for tiles */
-      cb->new( topo, obj );
-    }
-    if( FD_UNLIKELY( 0== strcmp( obj->name, "keyswitch" ) ) ) {
-      keyswitch_obj_id = obj->id;
-    }
-    // TODO add ASAN and MSAN poisoned memory before and after
-  }
-
-  /* The rest of this function an adoption of fd_topo_wksp_fill without
-     the wksp id checks.  I haven't looked into why they are needed */
-  for( ulong i=0UL; i<topo->link_cnt; i++ ) {
-    fd_topo_link_t * link = &topo->links[ i ];
-    link->mcache = fd_mcache_join( fd_topo_obj_laddr( topo, link->mcache_obj_id ) );
-    FD_TEST( link->mcache );
-    /* only saw this false for tile code */
-    if( FD_LIKELY( link->mtu ) ) {
-      link->dcache = fd_dcache_join( fd_topo_obj_laddr( topo, link->dcache_obj_id ) );
-      FD_TEST( link->dcache );
-    }
-  }
-
-  for( ulong i=0UL; i<topo->tile_cnt; i++ ) {
-    fd_topo_tile_t * tile = &topo->tiles[ i ];
-    tile->keyswitch_obj_id = keyswitch_obj_id;
-
-    tile->metrics = fd_metrics_join( fd_topo_obj_laddr( topo, tile->metrics_obj_id ) );
-    FD_TEST( tile->metrics );
-
-    for( ulong j=0UL; j<tile->in_cnt; j++ ) {
-      tile->in_link_fseq[ j ] = fd_fseq_join( fd_topo_obj_laddr( topo, tile->in_link_fseq_obj_id[ j ] ) );
-      FD_TEST( tile->in_link_fseq[ j ] );
-    }
-  }
-}
-
-
-static fd_topo_run_tile_t *
-find_run_tile( fd_drv_t * drv, char * name ) {
-  for( ulong i=0UL; drv->tiles[ i ]; i++ ) {
-    if( 0==strcmp( name, drv->tiles[ i ]->name ) ) return drv->tiles[ i ];
-  }
-  FD_LOG_ERR(( "tile %s not found", name ));
-}
-
-static fd_topo_tile_t *
-find_topo_tile( fd_drv_t * drv, char * name ) {
-  for( ulong i=0UL; drv->config.topo.tile_cnt; i++ ) {
-    if( 0==strcmp( name, drv->config.topo.tiles[ i ].name ) ) return &drv->config.topo.tiles[ i ];
-  }
-  FD_LOG_ERR(( "tile %s not found", name ));
-}
-
-static fd_topo_run_tile_t *
-tile_topo_to_run( fd_drv_t * drv, fd_topo_tile_t * topo_tile ) {
-  return find_run_tile( drv, topo_tile->name );
 }
 
 static void
